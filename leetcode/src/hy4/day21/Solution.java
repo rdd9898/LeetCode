bf7204a8568a7515e63e3e7b1b3fb599a2bf0d8c
root <1151154188@qq.com>
2022-05-05 23:17:51 +0800

    面试题05.07.配对交换

---
+++ leetcode/src/hy4/day21/Solution.java
@@ -0,0 +1,18 @@
+package hy4.day21;
+
+/**
+ * @program: LeetCode
+ * @description: 面试题05.07.配对交换
+ * @author: Mr.Hu
+ * @create: 2022-05-05 22:59
+ **/
+public class Solution {
+
+    public int exchangeBits(int num) {
+
+        return (0b01010101010101010101010101010101 & num) << 1
+                | (0b10101010101010101010101010101010 & num) >> 1;
+
+    }
+
+}
